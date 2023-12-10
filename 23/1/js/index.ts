3f91c3b8532d1a713d3be1b9b085194c5984f1a4
Igor Becker <igor_nyu@hotmail.com>
2023-12-09 22:01:09 -0300

    zzz

--- 23/1/js/index.ts
+++ 23/1/js/index.ts
@@ -2,7 +2,7 @@ import {readFileSync} from 'node:fs';
 import {resolve} from 'node:path';
 
 const isTest = !process.argv.includes('input');
-const filePath = resolve(__dirname, isTest ? 'example.txt' : 'input.txt');
+const filePath = resolve(__dirname, isTest ? 'example_2.txt' : 'input.txt');
 
 const file = readFileSync(filePath, 'utf8');
 
@@ -17,35 +17,80 @@ const TRANSLATE_NUMBER_MAP = {
   eight: 8,
   nine: 9,
 };
-
+const NUMBERS_WORDS = [
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+] as const;
 
 // no regex
 function part_one(file: string) {
-  const fileLines = file.split('\n').filter(Boolean)
+  const fileLines = file.split('\n').filter(Boolean);
 
-  const arr: string[] = []
+  const arr: string[] = [];
   for (let line of fileLines) {
-      const letters = line.split('')
-      const temp: string[] = []
+    const letters = line.split('');
+    const temp: string[] = [];
 
-      for (let letter of letters) {
-          if (!Number.isNaN(Number(letter))) {
-              temp.push(letter)
-          }
+    for (let letter of letters) {
+      if (!Number.isNaN(Number(letter))) {
+        temp.push(letter);
       }
+    }
 
-      arr.push(temp[0] + temp[temp.length - 1])
+    arr.push(temp[0] + temp[temp.length - 1]);
   }
 
   const sum = arr.reduce((prev, curr) => {
-      return Number(prev) + Number(curr)
-  }, 0)
+    return Number(prev) + Number(curr);
+  }, 0);
+
+  console.log(sum);
+}
+
+function part_two(file: string) {
+  const fileLines = file.split('\n');
+
+  // the problem is here
+  const reg = new RegExp(
+    '(one|two|three|four|five|six|seven|eight|nine|\\d)',
+    'gi',
+  );
+
+  const nums: string[] = [];
+
+  for (let line of fileLines) {
+    const match = line.match(reg);
+    const result: string[] = [];
+
+    if (match) {
+      result.push(match[0], match[match.length - 1]);
+    }
+
+    const translated: string[] = [];
+    for (let word of result) {
+      if (Number.isNaN(Number(word))) {
+        translated.push(String(TRANSLATE_NUMBER_MAP[word]));
+      } else {
+        translated.push(word);
+      }
+    }
+
+    nums.push(translated.join(''));
+  }
+
+  const sum = nums.filter(Boolean).reduce((prev, curr) => {
+    return Number(prev) + Number(curr);
+  }, 0);
 
   console.log(sum)
 }
 
-function part_two() {
-    const fileLines = file.split('\n')
-}
-
-part_one(file);
+// part_one(file);
+part_two(file);
