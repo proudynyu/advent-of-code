5c74fb42777f0c588710b585643621d7c0317e4f
Igor Becker <igor_nyu@hotmail.com>
2024-09-08 17:21:26 -0300

    update

--- 2015/05/main.exs
+++ 2015/05/main.exs
@@ -17,47 +17,42 @@ defmodule Resolver do
     |> String.split("\n")
   end
 
-  @spec not_allowed?(String.t())
-  defp not_allowed?(line) do
-    must_not_contain = ["ab", "cd", "pq", "xy"]
-    line
-      |> String.contains?(must_not_contain)
-  end
-
-  @spec has_double_vowels?(String.t(), list(String.t()))
-  defp has_double_vowels?(line, doubles_vowels) do
-    # TODO
-  end
-
   @spec create_vowels() :: list(String.t())
   defp create_vowels() do
     "aeiou"
       |> String.split()
+      |> Enum.filter(fn x -> String.length(x) > 0 end)
+      |> MapSet.new()
   end
 
-  @spec create_double_alphabet() :: list(String.t())
-  defp create_double_alphabet() do
-    Enum.map(65..90, fn n -> 
-      String.downcase(<<n>><><<n>>)
-    end)
+  @spec no_forbiden_substrings?(String.t()) :: Atom.t()
+  defp no_forbiden_substrings?(line) do
+    not (
+      line
+      |> String.contains?(["ab", "cd", "pq", "xy"])
+    )
   end
 
-  @spec has_double_alpha_letter?(String.t(), list(String.t()))
-  defp has_double_alpha_letter?(line, double_alpha) do
+  @spec has_three_vowels?(String.t(), list(String.t())) :: Atom.t()
+  defp has_three_vowels?(_line, _vowels) do
+    true
+  end
+
+  @spec has_double_alpha_letter?(String.t()) :: Atom.t()
+  defp has_double_alpha_letter?(_line) do
+    true
   end
 
   @spec part_one(String.t()) :: Integer.t()
   def part_one(filename) do
-    doubl_alpha = create_double_alphabet()
     vowels = create_vowels()
-
     filename
       |> read_file()
       |> split_file_line()
       |> Enum.filter(fn line ->
-          not not_allowed?(line)
-          and has_double_alpha_letter?(line, double_alpha)
-          and duplicate_vowels(line, vowels)
+          no_forbiden_substrings?(line)
+          and has_double_alpha_letter?(line)
+          and has_three_vowels?(line, vowels)
       end)
   end
 end
